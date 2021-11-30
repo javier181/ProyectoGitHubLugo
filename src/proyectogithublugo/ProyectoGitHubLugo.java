9e45840ba0621b75715bb380c16a95a0cfda3dfe
javier18 <javier18.lugo@gmail.com>
2021-11-29 22:15:10 -0600

    Prueba 1

---
+++ src/proyectogithublugo/ProyectoGitHubLugo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectogithublugo;
+
+/**
+ *
+ * @author javie
+ */
+public class ProyectoGitHubLugo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+for (int i = 0; i < 10000; i++){
+    System.out.println("Prueba de Git Hub Esfuerzo 1" + i);
+         }
+
+    }
+    
+}
